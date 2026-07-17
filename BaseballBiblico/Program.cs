735d67657c4880b5d0308119ab3a82a1a6820274
KevinJ240 <kevinjovel963@gmail.com>
2026-07-16 18:43:26 -0600

    le generé un txt para los errores futuros

--- BaseballBiblico/Program.cs
+++ BaseballBiblico/Program.cs
@@ -1,4 +1,31 @@
 ﻿using BaseballBiblico.Core;
 
-Game game = new Game();
-game.Run();
+try
+{
+    Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+
+    Game game = new Game();
+    game.Run();
+}
+catch (Exception ex)
+{
+    string rutaError = Path.Combine(
+        AppContext.BaseDirectory,
+        "error_inicio.txt"
+    );
+
+    File.WriteAllText(
+        rutaError,
+        $"""
+        Fecha: {DateTime.Now:yyyy-MM-dd HH:mm:ss}
+
+        Tipo: {ex.GetType().FullName}
+
+        Mensaje:
+        {ex.Message}
+
+        Detalles:
+        {ex}
+        """
+    );
+}
